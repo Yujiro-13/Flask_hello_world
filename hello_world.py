c76f3bf41241a1d40dc777face74d701010b5857
Yujiro Shito <s.yujiro613@gmal.com>
2024-03-25 11:06:48 +0900

    [Add] 画像表示、画像リンク先追加

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import numpy as np
 import pandas as pd
+from os.path import join, relpath
+from glob import glob
 
 app = Flask(__name__) #Flaskクラスのインスタンスを生成
 
@@ -26,12 +28,23 @@ def hello_world(): #hello_world関数を定義
     #return render_template('index.html', message=my_dict) #index.htmlに辞書型の変数を渡してレンダリング 
 
 
+#def index():
+    #num_list = np.arange(10)
+    #to_num_list = num_list.tolist() #numpy配列をリスト配列に変換しないとエラーが出る
+    #return render_template('index.html', message=to_num_list) #index.htmlにリスト配列を渡してレンダリング
+
 def index():
-    num_list = np.arange(10)
-    to_num_list = num_list.tolist() #numpy配列をリスト配列に変換しないとエラーが出る
-    return render_template('index.html', message=to_num_list) #index.htmlにリスト配列を渡してレンダリング
-
-
+    path = './static'
+    image_names = [relpath(x, path) for x in glob(join(path, '*.jpg'))]
+    # static内の.pngファイルがimage_namesに格納されます。
+    my_list = []
+    for image in image_names:
+        my_dic = {}
+        my_dic['image_name'] = 'static/' + image
+        my_dic['link_url'] = 'https://github.com/Yujiro-13/Flask_hello_world/assets/114651033/208d4f48-fa54-460c-9938-bdc188b5d2ef'
+        # 飛び先のURLを何個も確保できなかったのでどの画像に対しても同じURLに飛ぶようにしました。
+        my_list.append(my_dic)
+    return render_template('index.html', message = my_list) #/indexにアクセスが来たらtemplates内のindex.htmlが開きます
 
 if __name__ == '__main__': #このスクリプトが直接実行された場合にのみ、以下のコードが実行される
     app.run(host="0.0.0.0", port=8000, debug=True) #Flaskアプリケーションを起動
